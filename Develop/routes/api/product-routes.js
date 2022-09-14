68b6b41213f04e8b78ed5d9b9d40cfd395a641c8
R Greene <greenbert81@gmail.com>
2022-09-14 00:31:51 -0700

    created get routes for all and single products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,18 +1,54 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+
+const { 
+  Product, 
+  Category, 
+  Tag, 
+  ProductTag,
+} = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag
+        },
+        {
+          model: ProductTag
+        },
+      ]
+    });
+    res.json(products);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      return res.status(400).json({ error: "Could not find matching product!"});
+    }
+    res.json(product);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 // create new product
