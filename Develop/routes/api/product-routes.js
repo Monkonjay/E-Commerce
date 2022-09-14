087ff2317617e58a5e289e70e21d7b01de4a9f40
R Greene <greenbert81@gmail.com>
2022-09-14 00:51:02 -0700

    prduct delete route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -125,8 +125,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!product) {
+      res.status(404).json({ error: "Could not find matching product!"});
+      return;
+    }
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
